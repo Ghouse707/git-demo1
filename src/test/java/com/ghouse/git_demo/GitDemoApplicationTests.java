73e19d5bece46caad15a5ce07c10d55a28aff814
Ghouse707 <ghousepashaashrafi12@gmail.com>
2025-07-10 18:46:25 +0530

    added to local repo

---
+++ src/test/java/com/ghouse/git_demo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghouse.git_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
